4d9dab05012d530779c1a432df2a2390a9f8c597
Guillaume Poirier-Morency <guillaumepoiriermorency@gmail.com>
2012-08-10 08:19:03 -0400

    Petits changfements

--- classes/urlang.php
+++ classes/urlang.php
@@ -5,7 +5,6 @@ defined('SYSPATH') or die('No direct script access.');
 class Urlang {
 
     private static $instance;
-    
     private $_config;
 
     public static function instance() {
@@ -13,27 +12,35 @@ class Urlang {
     }
 
     private function __construct() {
+        
     }
-    
+
     /**
      * Returns a value based on a key in the associative array. 
      * @param type $lang
      */
-    private function get_value($lang = null) {
-        
+    private function get_value($key, $lang = null) {
+
         $translation_array = Kohana::$config->load($lang ? $lang : i18n::lang());
-        
-        
-        
+
+        return array_key_exists($translation_array, $key) ? $translation_array[$key] : $key;
     }
-    
+
     /**
      * Scans all lang, revert associative array and tries to match a key.
      */
-     private function get_key() {
-         
-         
-     }
+    private function get_key($translated_value) {
+        $configs = Kohana::$config->load();
+        foreach ($configs as &$lang => &$values) {
+            foreach ($values as &$key => &$value) {
+                if ($translated_value === $value) {
+                    return $key;
+                }
+            }
+        }
+        return $translated_value;
+        throw new Kohana_Exception("Aucune valeur d'origine trouvée pour la valeur traduite :value", array(":value" => $translated_value));
+    }
 
     public function uri_to_translation($uri) {
 
